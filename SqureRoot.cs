fb824807e211dcbfafb712c6c0dc20376e354fbf
taralpanchal <79502098+taralpanchal@users.noreply.github.com>
2021-04-05 09:58:28 +0530

    Square Root

---
+++ SqureRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+
+            Console.WriteLine("Enter Number : ");
+            var number = Convert.ToInt32(Console.ReadLine()); 
+             
+            for(int i = 2; i < number; i++)
+            {
+                c = number / i;
+                if( c * c == number )
+                {
+                    Console.WriteLine("Square Root is : " + c);
+                }
+            }
+
+        }
+    }
+}
