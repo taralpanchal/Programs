76fe8f6f75bc7d7bddea7df831d62d0d2f8f3d72
taralpanchal <79502098+taralpanchal@users.noreply.github.com>
2021-04-05 09:57:21 +0530

    Swap Number

---
+++ SwapingNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwapingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int temp = 0;
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("a : " + a);
+            Console.WriteLine("b : " + b);
+        }
+    }
+}
