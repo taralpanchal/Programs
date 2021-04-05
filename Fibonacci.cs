e4efc1e41a92aace49a46737c65edcdccd605754
taralpanchal <79502098+taralpanchal@users.noreply.github.com>
2021-04-05 10:00:16 +0530

    Fibonacci Series

---
+++ Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currnum = 0;
+            int prevnum = 1;
+
+            Console.WriteLine("Enter Number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= number; i++)
+            {
+                int sum = currnum + prevnum;
+                currnum = prevnum;
+                prevnum = sum;
+                Console.WriteLine("Fibonacci Series : " + prevnum);
+            }
+        }
+    }
+}
