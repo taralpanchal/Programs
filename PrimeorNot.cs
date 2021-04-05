168cf5c53ac9f61d9acd833f641bf92e53d701c1
taralpanchal <79502098+taralpanchal@users.noreply.github.com>
2021-04-05 10:29:29 +0530

    Number is Prime or Not Prime

---
+++ PrimeorNot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PrimeOrNotPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime == true)
+            {
+                Console.WriteLine(number + " is prime");
+            }
+            else
+            {
+                    Console.WriteLine(number + " is not prime");
+            }
+        }
+    }
+}
