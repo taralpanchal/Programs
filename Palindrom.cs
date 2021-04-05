de214523ca76b83e9fde13cc39a4c93f3a185487
taralpanchal <79502098+taralpanchal@users.noreply.github.com>
2021-04-05 09:59:14 +0530

    Palindrom

---
+++ Palindrom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Digit : ");
+            var digit = Convert.ToInt64(Console.ReadLine());
+
+            var digitstr = digit.ToString();
+            var leng = digitstr.Length;
+            var i = leng - 1;
+            var revesedigit = "";
+
+            while (i >= 0)
+            {
+                revesedigit = revesedigit + digitstr[i];
+                i--;
+            }
+            if(Convert.ToInt64(revesedigit) == digit)
+            {
+                Console.WriteLine(digit + " is Palindrom");
+            }
+            else
+            {
+                Console.WriteLine(digit + " is Not Palindrom");
+            }
+            
+        }
+    }
+}
